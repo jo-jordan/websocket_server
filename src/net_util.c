4a8cf32f153b0e2cf42f8e0a198431e96f15db1f
lzjlxebr <tyyzmyd@gmail.com>
2021-06-11 14:15:25 +0800

    Fix a linux net address out error

--- src/net_util.c
+++ src/net_util.c
@@ -5,7 +5,11 @@
 #include "net_util.h"
 
 const char* get_conn_addr(struct sockaddr_in *addr) {
-    char buff[64];
+    // An AF_INET address (which has the form xxx.xxx.xxx.xxx) uses a
+    // maximum of 4x3 characters plus 3 delimiting . chars which sums
+    // up to 15 chars plus 1 additional char used as 0-terminator to
+    // make it a C-"string"
+    char buff[16];
     return inet_ntop(AF_INET, &addr->sin_addr, buff, sizeof(buff));
 }
 
